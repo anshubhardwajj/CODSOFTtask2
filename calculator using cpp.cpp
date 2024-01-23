e13fc4162c022a67c9c5dbe0d5fb9b440d1d03c4
anshubhardwajj <157483712+anshubhardwajj@users.noreply.github.com>
2024-01-23 19:51:57 +0530

    Add files via upload

---
+++ calculator using cpp.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+float x;
+cout<<"enter 1st number : ";
+cin>>x;
+char op;
+cout<<"enter (+,-,*,/) ";
+cin>> op;
+float y;
+cout<< "enter 2nd number: ";
+cin>>y;
+if(op=='+'){
+        cout<<"the sum is: ";
+    cout<<x+y;
+}
+if(op=='-'){
+    cout<<"the difference is: ";
+    cout<<x-y;
+}
+if(op=='*'){
+    cout<<"the product is : ";
+    cout<<x*y;
+}
+if(op=='/'){
+    cout<< "the quotient is : ";
+    cout<<x/y;
+}
+}
